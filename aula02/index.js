376701cf1c4d99a9ad551855953a4dd073973e16
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-03-28 11:29:48 -0300

    Update Course

--- aula02/index.js
+++
@@ -1,4 +0,0 @@
-var p = document.createElement('p');
-var ola = "Aprendendo com DevDojo - O Samurai da Tecnologia";
-p.textContent = ola;
-document.body.appendChild(p);
--- aula02/index.ts
+++
@@ -1,4 +0,0 @@
-var p = document.createElement('p');
-var ola: string = "Aprendendo com DevDojo - O Samurai da Tecnologia";
-p.textContent = ola;
-document.body.appendChild(p);
