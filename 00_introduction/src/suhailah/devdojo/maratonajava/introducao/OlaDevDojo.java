1d90523c55abef72e7c63868757bc716c26c70fa
Suhailah Alves de Moraes da Conceicao <suhailah@ciandt.com>
2022-03-29 12:21:10 -0300

    Tipos Primitivos

---
+++ 00_introduction/src/suhailah/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,7 @@
+package suhailah.devdojo.maratonajava.introducao;
+
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Olá, Desenvolvedor");
+    }
+}
